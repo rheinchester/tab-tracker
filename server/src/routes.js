447fd8ecee6f3c0cc47dcdbb7c3070b9311731ae
Jacob Okoro <jaycobokoro@gmail.com>
2018-06-06 08:16:01 +0200

    Just recreated authenticationControllerPolicy.js. It's now working

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,12 +3,15 @@
  * Pass in app and the function attaches all the endpoints
  * to the application
  */
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 const AuthenticationController = require('./controllers/AuthenticationController')
 module.exports = (app) => {
-    app.get('/', ()=>{
-        console.log('Hello world')
-    } )
+    app.get('/', (req, res)=>{
+        res.send('Hello world')
+    })
+     
     app.post('/register', 
+    AuthenticationControllerPolicy.register,
     AuthenticationController.register
     )
 }
