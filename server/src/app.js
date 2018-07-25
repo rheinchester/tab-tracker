1c9cfa279a48ddca61a08789895ff337f02c0236
Jacob Okoro <jaycobokoro@gmail.com>
2018-07-25 21:50:50 +0200

    Improved documentation and format of songController.js and app.js .

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,12 @@
+
+/**
+ * -- body-parser: allows app to process json easily
+ * -- cors: enables http query at the backend
+ * -- morgan: allows us to see which device hit the server
+ */
 const express = require('express')
-
-// body-parser allows app to process json easily
 const bodyParser = require('body-parser')
-
-// cors enables http query at the backend
 const cors = require('cors')
-
-// morgan allows us to see which device hit the server
 const morgan = require('morgan')
 
 //returning sequelize object from models
@@ -22,12 +22,14 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+//route.js file
 require('./routes')(app)
 
 
 /**
  *  sequelize.sync() connects sequelize to whatever database you have it configured
  *  for. Then starts server from a port defined in ./config/config.js
+ *  
  * NB: {force:true} clears the whole db
  */
 sequelize.sync()
--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -1,53 +1,53 @@
-const {Song} = require('../models')
+const { Song } = require('../models')
 // NB: {force:true} clears the whole db. So in case server misbehaves try it. 
 // Figure out a better solution later
 
 module.exports = {
-    async show (req, res){
-        try {
-            const songs = await Song.findById(req.params.songId)
-            res.send(songs)
-        } catch (err) {
-            res.status(500).send({
-                error: 'An error occured trying to fetch songs'
-            })
-        }
-    },
-    async index(req, res){
-        try {
-            const songs = await Song.findAll({
-                limit: 15
-            })
-            res.send(songs)
-        } catch (err) {
-            res.status(500).send({
-                error: 'An error occured trying to fetch songs'
-            })
-        }
-    },
-    async post (req, res) {
-        try {
-            const song = await Song.create(req.body)
-            res.send(song)
-        } catch(err){
-            res.status(500).send({
-                error: 'An error has occured trying to create songs'
-            })
-        }
-    },
-    async put (req, res) {
-        try {
-            const song = await Song.update(req.body, {
-                where: {
-                    id: req.params.songId
-                }
-            })
-            res.send(req.body)
-        } catch(err){
-            res.status(500).send({
-                error: 'An error has occured trying to create songs'
-            })
-        }
+  async show(req, res) {
+    try {
+      const songs = await Song.findById(req.params.songId)
+      res.send(songs)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error occured trying to fetch songs'
+      })
     }
+  },
+  async index(req, res) {
+    try {
+      const songs = await Song.findAll({
+        limit: 15
+      })
+      res.send(songs)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error occured trying to fetch songs'
+      })
+    }
+  },
+  async post(req, res) {
+    try {
+      const song = await Song.create(req.body)
+      res.send(song)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to create songs'
+      })
+    }
+  },
+  async put(req, res) {
+    try {
+      const song = await Song.update(req.body, {
+        where: {
+          id: req.params.songId
+        }
+      })
+      res.send(req.body)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to create songs'
+      })
+    }
+  }
 }
 
