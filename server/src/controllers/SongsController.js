7bc775f82ad83bef6e814640fa99c4cc8c75f195
Jacob Okoro <jaycobokoro@gmail.com>
2018-10-25 20:00:44 +0200

    Search feature complete

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -15,10 +15,26 @@ module.exports = {
   },
   async index(req, res) {
     try {
-      const songs = await Song.findAll({
+      let songs = null
+      const search = req.query.search
+      if (search) {
+        songs = await Song.findAll({
+          where: {
+            $or: [
+              'title', 'artist', 'genre', 'album'
+            ].map(key => ({
+              [key]: {
+                $like: `%${search}%`
+              }
+            }))
+          }
+        })
+      } else {
+         const songs = await Song.findAll({
         limit: 15
       })
-      res.send(songs)
+    }
+    res.send(songs)
     } catch (err) {
       res.status(500).send({
         error: 'An error occured trying to fetch songs'
@@ -50,4 +66,3 @@ module.exports = {
     }
   }
 }
-
